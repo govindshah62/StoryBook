c8ca58d84a38ca802a1206a8e4eb87aa884060d9
govindshah62 <govindshah2010@gmail.com>
2020-08-17 22:13:36 +0530

    commit

--- config/dbconfig.js
+++
@@ -1,5 +0,0 @@
-if(process.env.NODE_ENV ==='production'){
-    module.exports = {mongoURI:'mongodb+srv://evil:evil123@cluster0.ehcjy.mongodb.net/StoryBook?retryWrites=true&w=majority'}
-}else{
-    module.exports= {mongoURI:'mongodb://localhost/StoryBook'}
-}
